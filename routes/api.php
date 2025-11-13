921fe3c68b8f3235f03a24de492f3ec532a6df4e
John Paul Quintana <jpquintana2024@gmail.com>
2025-11-13 12:29:14 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,13 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Test endpoint is working!',
+        'time' => now(),
+    ]);
+});
 Route::post('/auth/google-login', [AuthController::class, 'googleLogin']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
